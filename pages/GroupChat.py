a9b68e01fbf59e6cd3f9a2fff5efb02ba83e9e6f
‘yangxiuhong’ <yangxiuhong@chinasoftinc.com>
2019-08-01 16:23:35 +0800

    适配企业群双机用例 修改群聊预置页面 群聊预置选择联系人数据

--- pages/GroupChat.py
+++ pages/GroupChat.py
@@ -144,6 +144,12 @@ class GroupChatPage(BaseChatPage):
                 self.click_send_button()
                 time.sleep(2)
 
+    @TestLogger.log()
+    def click_start_call_button(self):
+        """点击开始呼叫按钮 """
+        self.click_element((MobileBy.IOS_PREDICATE, 'name CONTAINS "呼叫"'))
+
+
     @TestLogger.log('点击输入框')
     def click_input_box(self):
         self.click_element(self.__locators['说点什么'])
